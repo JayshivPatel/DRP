9ba2ca08a0dc22ffdc0b1ded8643f2f08544af60
Jayshiv Patel <jdp221@ic.ac.uk>
2023-06-01 17:02:29 +0100

    Fix Pipeline fix imports

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,10 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import "dotenv/config";
+import twilio from "twilio";
 
-require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountSid, authToken);
+const client = twilio(accountSid, authToken);
 const message =
   "Dear PATIENT, we regret to inform you that the clinic is currently running 45 minutes late. \n If you can no longer make this, please phone the surgery to cancel your appointment";
 
