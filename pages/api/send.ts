7011d92ca7fadcb6dc5dba34f3c0b60797ff7795
Jayshiv Patel <jdp221@ic.ac.uk>
2023-06-01 16:38:22 +0100

    Fix Pipeline - remote old verification codes and env file

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,12 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import 'dotenv/config'
 
-//I have created a separate env file (these should be referenced with process.env.TOKEN)
-//This doesn't work so left hardcoded for now.
 require("dotenv").config();
-const accountSid = "ACd0b0e732ed8d0b2c4fa10de618ad2945";
-const authToken = "e7dbbf2b0de00c1972be59a04d834443";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
-var message =
+const message =
   "Dear PATIENT, we regret to inform you that the clinic is currently running 45 minutes late. \n If you can no longer make this, please phone the surgery to cancel your appointment";
 
 export default async function handler(
