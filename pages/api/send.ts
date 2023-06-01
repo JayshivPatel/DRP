45be23b0d1dab6e0b4d5fab025b7a93328e1f63c
Saleem Rashid <saleem.rashid20@imperial.ac.uk>
2023-06-01 21:59:00 +0100

    api/send: Abstract SMS provider to sendSms function

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,18 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import twilio from "twilio";
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
-const message =
-  "Dear PATIENT, we regret to inform you that the clinic is currently running 45 minutes late. \n If you can no longer make this, please phone the surgery to cancel your appointment";
+import { sendSms } from "../../server/sms";
+
+const message = `Dear PATIENT, we regret to inform you that the clinic is currently running 45 minutes late.
+If you can no longer make this, please phone the surgery to cancel your appointment`;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  client.messages
-    .create({ body: message, from: "+447862128714", to: "+447770160480" })
-    .then((message) => console.log(message.sid));
-  res.status(200);
+  const { recipient } = req.query;
+
+  if (typeof recipient !== "string") {
+    res.status(400).json({});
+    return;
+  }
+
+  await sendSms(recipient, message);
+
+  res.status(200).json({});
 }
