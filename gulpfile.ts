efb5354873902869c34c82280e26d3b867cf1d66
Saleem Rashid <saleem.rashid20@imperial.ac.uk>
2023-06-01 11:27:30 +0100

    gitlab-ci: Fix test and deploy with Postgres

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,31 +1,27 @@
 const gulp = require("gulp");
 
+const dotenv = require("dotenv");
 const execa = require("execa");
 const fs = require("fs/promises");
 const waitOn = require("wait-on");
 
 function npx(command: string, args: String[], options?: any): any {
-  return execa(
-    command,
-    args,
-    Object.assign(
-      {
-        preferLocal: true,
-        stderr: "inherit",
-        stdout: "inherit",
-      },
-      options
-    )
-  );
+  return execa(command, args, {
+    preferLocal: true,
+    stderr: "inherit",
+    stdout: "inherit",
+    ...options,
+  });
 }
 
-async function vercel(...args: string[]): Promise<string> {
+async function vercel(args: string[], options?: any): Promise<string> {
   const maybeToken = process.env.VERCEL_TOKEN
     ? [`--token=${process.env.VERCEL_TOKEN}`]
     : [];
 
   const { stdout } = await npx("vercel", [...maybeToken, ...args], {
     stdout: "pipe",
+    ...options,
   });
   return stdout;
 }
@@ -40,12 +36,14 @@ gulp.task("build:expo", async () => {
 });
 
 gulp.task("build:next", async () => {
+  await npx("prisma", ["generate"]);
   await npx("next", ["build"]);
 });
 
 gulp.task("build", gulp.series("build:expo", "build:next"));
 
 function escapeShell(s: string): string {
+  /* All characters are allowed within single quotes, except single quotes */
   return "'" + s.replaceAll("'", "'\\''") + "'";
 }
 
@@ -53,9 +51,18 @@ async function deploy(production: boolean) {
   const environment = production ? "production" : "preview";
   const maybeProd = production ? ["--prod"] : [];
 
-  await vercel("pull", "--yes", `--environment=${environment}`);
-  await vercel("build", ...maybeProd);
-  const url = await vercel("deploy", ...maybeProd, "--prebuilt");
+  /* Pull environment variables for local build */
+  await vercel(["pull", "--yes", `--environment=${environment}`]);
+  const env = dotenv.parse(
+    await fs.readFile(`.vercel/.env.${environment}.local`)
+  );
+
+  /* Build application locally */
+  await vercel(["build", ...maybeProd], { env });
+  /* Deploy database migrations */
+  await npx("prisma", ["migrate", "deploy"], { env });
+  /* Deploy application */
+  const url = await vercel(["deploy", ...maybeProd, "--prebuilt"]);
 
   /* Write deployment URL to dotenv file for GitLab CI */
   await fs.writeFile("deploy.env", `DEPLOYMENT_URL=${escapeShell(url)}\n`);
