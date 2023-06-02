ba027ecad1a2b2df8e32a66e1eeb7f0ae035731c
Saleem Rashid <saleem.rashid20@imperial.ac.uk>
2023-06-02 11:22:21 +0100

    App: Change colour of notification

--- App.tsx
+++ App.tsx
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   notification: {
-    backgroundColor: "#f9c2ff",
+    backgroundColor: "#ff0000",
     marginVertical: 16,
     padding: 20,
   },
