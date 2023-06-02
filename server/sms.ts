2617391afd51766ad3fdb8642410e3ff7c32c75d
Saleem Rashid <saleem.rashid20@imperial.ac.uk>
2023-06-02 02:29:51 +0100

    server/sms: Lazy initialize Twilio client for tests

--- server/sms.ts
+++ server/sms.ts
@@ -1,13 +1,18 @@
 import { Twilio } from "twilio";
 
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
-const client = new Twilio(accountSid, authToken);
+/* Lazy initialize client for tests */
+const getClient = (function () {
+  let client: Twilio | null = null;
+
+  return () => (client = client || new Twilio(accountSid, authToken));
+})();
 
 export async function sendSms(to: string, body: string) {
-  await client.messages.create({
+  await getClient().messages.create({
     from: phoneNumber,
     to,
     body,
