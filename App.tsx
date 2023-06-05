508041c763e8555fc91ff675d6505cac0ad855ac
mh1021 <musawer.hussain21@imperial.ac.uk>
2023-06-05 23:15:07 +0100

    Added more booking appointment features

--- App.tsx
+++ App.tsx
@@ -18,6 +18,8 @@ export default function App() {
           <Clinic></Clinic>
         </View>
       </View>
+
+      
     </PaperProvider>
   );
 }
--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -33,9 +33,13 @@ export default class Schedule extends React.Component {
       outerHeight: 50,
     };
 
-    const modalViewStyle = {
+    const modalOneViewStyle = {
       paddingVertical: 50,
     };
+
+    const modalTwoViewStyle = {
+      paddingVertical: 50,
+    }
 
     return (
       <PaperProvider>
@@ -58,7 +62,7 @@ export default class Schedule extends React.Component {
             onDismiss={() => this.setState({ showModalOne: false })}
             contentContainerStyle={containerStyle}
           >
-            <View style={modalViewStyle}>
+            <View style={modalOneViewStyle}>
               <Text style={{ marginBottom: 30 }}>
                 Random stuff here More stuff even more stuff waffling
               </Text>
@@ -76,11 +80,16 @@ export default class Schedule extends React.Component {
             onDismiss={() => this.setState({ showModalTwo: false })}
             contentContainerStyle={containerStyle}
           >
-            <View>
-              <Text>random stuff</Text>
+            <View style={modalTwoViewStyle}>
+              <Text style={{marginBottom: 30}}>random stuff</Text>
+              <Button
+                title={`Book Appointment for ${selectedEvent}`}
+                onPress={() => alert("Appointment Booked")}
+              />
             </View>
           </Modal>
         </Portal>
+
       </PaperProvider>
     );
   }
