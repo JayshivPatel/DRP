b5afc1cb45ae66b5aabc50eafc4026419bac83e3
mh1021 <musawer.hussain21@imperial.ac.uk>
2023-06-05 18:16:34 +0100

    Added calender to homepage

--- App.tsx
+++ App.tsx
@@ -1,22 +1,24 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import Clinic from "./components/Clinic";
 import { PaperProvider } from "react-native-paper";
+import { Calendar } from "@fullcalendar/core";
+import { Calendar as FullCalendar } from "react-native-calendars";
 
 export default function App() {
   return (
     <PaperProvider>
-      <View
-        style={[
-          styles.container,
-          {
-            flexDirection: "row",
-          },
-        ]}
-      >
-        <Clinic></Clinic>
-        <Clinic></Clinic>
-        <Clinic></Clinic>
-        <Clinic></Clinic>
+      <View style={styles.container}>
+        <View style={styles.calendarContainer}>
+          <FullCalendar/>
+        </View>
+
+        <View style={styles.clinicContainer}>
+          <Clinic></Clinic>
+          <Clinic></Clinic>
+          <Clinic></Clinic>
+          <Clinic></Clinic>
+        </View>
+      
       </View>
     </PaperProvider>
   );
@@ -26,8 +28,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
     padding: 15,
+    flexDirection: "row",
   },
+  
+  calendarContainer: {
+    marginRight: 15,
+    flex: 1,
+  },
+
+  clinicContainer: {
+    flexDirection: "row",
+    marginLeft: 10,
+    flex: 3,
+  }
 });
+
+
