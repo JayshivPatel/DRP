b10d6b9295586f2c954221f32792d4ab5f27fdf9
mh1021 <musawer.hussain21@imperial.ac.uk>
2023-06-05 21:35:24 +0100

    Added a modal for the event info

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ export default function App() {
         <View style={styles.calendarContainer}>
           <FullCalendar />
         </View>
-
         <View style={styles.clinicContainer}>
           <Clinic></Clinic>
           <Clinic></Clinic>
--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -3,33 +3,63 @@ import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import timeGridPlugin from "@fullcalendar/timegrid"; // a plugin!
 import { EventClickArg } from "@fullcalendar/core";
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import { Modal, PaperProvider, Portal } from "react-native-paper";
+import { TouchableOpacity } from "react-native";
 
 export default class Schedule extends React.Component {
+  state = {
+    showModal: false,
+    selectedEvent: null,
+  };
+
+  openEventDetailsOnClick = (payload: EventClickArg) => {
+    this.setState({ selectedEvent: payload.event });
+    this.setState({ showModal: true });
+  };
+
   render() {
+    const { showModal, selectedEvent } = this.state;
+
+    const containerStyle = {
+      backgroundColor: "white",
+      padding: 20,
+      innerHeight: 500,
+      outerHeight: 50,
+    };
+
     return (
-      <FullCalendar
-        plugins={[timeGridPlugin, interactionPlugin]}
-        initialView="timeGridDay"
-        headerToolbar={false}
-        height="auto"
-        expandRows={true}
-        dayHeaders={false}
-        allDaySlot={false}
-        eventClick={openEventDetailsOnClick}
-        dateClick={openBookingScreen}
-        events={events}
-      />
+      <PaperProvider>
+        <FullCalendar
+          plugins={[timeGridPlugin, interactionPlugin]}
+          initialView="timeGridDay"
+          headerToolbar={false}
+          height="auto"
+          expandRows={true}
+          dayHeaders={false}
+          allDaySlot={false}
+          eventClick={this.openEventDetailsOnClick}
+          dateClick={openBookingScreen}
+          events={events}
+        />
+
+        <Portal>
+          <Modal
+            visible={showModal}
+            onDismiss={() => this.setState({ showModal: false })}
+            contentContainerStyle={containerStyle}
+          >
+            <view>
+              <text>Random stuff here More stuff even more stuff waffling</text>
+            </view>
+          </Modal>
+        </Portal>
+      </PaperProvider>
     );
   }
 }
 
 function openBookingScreen(payload: DateClickArg) {
   alert("Should allow us to book here");
-  payload;
-}
-
-function openEventDetailsOnClick(payload: EventClickArg) {
-  alert("Appointment information should pop up here?");
   payload;
 }
 
