c9daf38ce2e56557c7ae50d7d2ad3fc19c25b5e5
Saleem Rashid <saleem.rashid20@imperial.ac.uk>
2023-06-08 18:22:21 +0100

    MiniCalender: Fix selected date highlight

--- components/MiniCalender.tsx
+++ components/MiniCalender.tsx
@@ -1,26 +1,33 @@
 import React from "react";
+import { View } from "react-native";
 import FullCalendar from "@fullcalendar/react";
 import { DateClickArg } from "@fullcalendar/interaction";
 import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import { PaperProvider } from "react-native-paper";
 
-type Props = { changeDate: (date: string) => void };
+export default function MiniCalender(props: {
+  changeDate: (date: string) => void;
+}) {
+  const [now, setNow] = React.useState(new Date());
 
-export default class MiniCalender extends React.Component<Props> {
-  changeDaySelected = (payload: DateClickArg) => {
-    this.props.changeDate(payload.dateStr);
-  };
-  render() {
-    return (
-      <PaperProvider theme={{ version: 2 }}>
+  function changeDaySelected(payload: DateClickArg) {
+    setNow(payload.date);
+    props.changeDate(payload.dateStr);
+  }
+
+  return (
+    <PaperProvider theme={{ version: 2 }}>
+      {/* Workaround for https://github.com/fullcalendar/fullcalendar/issues/6322 */}
+      <View key={now.toDateString()}>
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
+          now={now}
           nowIndicator={true}
-          dateClick={this.changeDaySelected}
+          dateClick={changeDaySelected}
           contentHeight={400}
         />
-      </PaperProvider>
-    );
-  }
+      </View>
+    </PaperProvider>
+  );
 }
