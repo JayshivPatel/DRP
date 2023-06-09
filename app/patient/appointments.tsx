c59ce0335859e26f52aad792e61d2094d9a83f76
Oleg Tretieu <ot321@ic.ac.uk>
2023-06-09 12:35:45 +0100

    Initial commit: Add pages for patient appointments and dashboard

---
+++ app/patient/appointments.tsx
@@ -0,0 +1,5 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+export default function Appointments() {
+  return <Text>Appointments</Text>;
+}
---
+++ app/patient/dashboard.tsx
@@ -0,0 +1,5 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+export default function PatientApp() {
+  return <Text>Dashboard</Text>;
+}
