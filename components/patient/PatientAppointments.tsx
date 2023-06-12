06a223a23b5b8ced20be5f80526b627b3bf3ea30
mh1021 <musawer.hussain21@imperial.ac.uk>
2023-06-12 03:00:38 +0100

    Wired up database to past appointments and upcoming appointments and sorted each section the relevant way

--- components/patient/PatientAppointments.tsx
+++ components/patient/PatientAppointments.tsx
@@ -3,6 +3,7 @@ import Appointment from "./PatientAppointment";
 import { ScrollView } from "react-native";
 import { useEffect, useState } from "react";
 import { Appointment as App, useAppointments } from "../../lib/api";
+import { format } from "date-fns";
 
 export default function Appointments() {
   const { data: apps, isLoading } = useAppointments({
@@ -10,12 +11,20 @@ export default function Appointments() {
     includePatient: true,
   });
 
-  const sortedApps = apps?.sort((app1, app2) => {
-    const date1 = app1.clinic?.date ? new Date(app1.clinic?.date) : new Date(0);
-    const date2 = app2.clinic?.date ? new Date(app2.clinic?.date) : new Date(0);
+  function sortAppointments(reverse: boolean) {
+    apps?.sort((app1, app2) => {
+      const date1 = app1.clinic?.date ? new Date(app1.clinic?.date) : new Date(0);
+      const date2 = app2.clinic?.date ? new Date(app2.clinic?.date) : new Date(0);
 
-    return date2.getTime() - date1.getTime();
-  });
+      if (reverse) {
+        return date2.getTime() - date1.getTime();
+      }
+
+      return date1.getTime() - date2.getTime();
+    });
+
+    return apps;
+  }
 
   return (
     <PaperProvider>
@@ -30,7 +39,11 @@ export default function Appointments() {
       <ScrollView style={{ height: "50%" }}>
         <Card>
           <Card.Content>
-            {sortedApps?.map((app) => (
+            {sortAppointments(false)?.filter((app) => {
+              const currentDate = new Date();
+              const appDate = new Date(app.clinic?.date ? app.clinic?.date : 0);
+              return appDate.getTime() >= currentDate.getTime();
+            }).map((app) => (
               <>
                 <Appointment {...app} />
                 <Divider />
@@ -46,12 +59,16 @@ export default function Appointments() {
       <ScrollView style={{ height: "50%" }}>
         <Card>
           <Card.Content>
-            <Appointment
-              startTime="09:00"
-              endTime="09:30"
-              notes="Broken Toe"
-              id={10}
-            />
+          {sortAppointments(true)?.filter((app) => {
+              const currentDate = new Date();
+              const appDate = new Date(app.clinic?.date ? app.clinic?.date : 0);
+              return appDate.getTime() < currentDate.getTime();
+            }).map((app) => (
+              <>
+                <Appointment {...app} />
+                <Divider />
+              </>
+            ))}
           </Card.Content>
         </Card>
       </ScrollView>
