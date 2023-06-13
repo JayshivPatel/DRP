f6ff1e99597efd659a793f098331152365dc7e96
Saleem Rashid <saleem.rashid20@imperial.ac.uk>
2023-06-13 13:51:18 +0100

    Remove old receptionist UI

--- app/receptionist.tsx
+++ app/receptionist.tsx
@@ -1,81 +1,102 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import ClinicInfo from "../components/ClinicInfo";
-import {
-  PaperProvider,
-  Button,
-  Portal,
-  Modal,
-  Searchbar,
-  Card,
-} from "react-native-paper";
-import { SetStateAction, useState } from "react";
-import MiniCalender from "../components/MiniCalender";
-import Toolbar from "../components/Toolbar";
-import PatientInfo from "../components/PatientInfo";
+import * as React from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import { DefaultTheme, PaperProvider, Surface } from "react-native-paper";
+import { OrderedSet } from "immutable";
 
-import { Patient, createClinic, useClinics } from "../lib/api";
-import type { Clinic } from "../lib/api";
-const today = new Date().toISOString().split("T")[0];
+import type { Clinic, Patient } from "../lib/api";
+
+import ClinicToolbar from "../components/ClinicToolbar";
+import ClinicViews from "../components/ClinicViews";
+import PatientToolbar from "../components/PatientToolbar";
+
+import materialColors from "../material-colors.json";
+import ChangePatientDialog from "../components/ChangePatientDialog";
+import { DatePickerModal } from "react-native-paper-dates";
 
 export default function Receptionist() {
-  const [searchVisible, setSearchVisible] = useState(false);
-  const [clinicVisible, setClinicVisible] = useState(false);
-  const [searchquery, setSearchquery] = useState("");
-  const [patient, setPatient] = useState<Patient | undefined>(undefined);
-  const [date, setDate] = useState(today);
+  const [changeDateVisible, setChangeDateVisible] = React.useState(false);
+  const [changePatientVisible, setChangePatientVisible] = React.useState(false);
 
-  const onChangeSearch = (query: SetStateAction<string>) =>
-    setSearchquery(query);
-  const showSearchModal = () => setSearchVisible(true);
-  const hideSearchModal = () => setSearchVisible(false);
+  function formatDate(date: Date): string {
+    /* TODO(saleem): Change to date library */
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
 
-  const { data: clinics, mutate: mutateClinics } = useClinics({
-    date: date,
-  });
+  const [date, setDate] = React.useState(formatDate(new Date()));
+  const [selectedClinics, setSelectedClinics] = React.useState(
+    OrderedSet<Clinic["id"]>()
+  );
+  const [patient, setPatient] = React.useState<Patient | undefined>(undefined);
 
-  function updateDate(date: string) {
-    setDate(date);
+  function openChangeDate() {
+    setChangeDateVisible(true);
+  }
+
+  function closeChangeDate() {
+    setChangeDateVisible(false);
+  }
+
+  function openChangePatient() {
+    setChangePatientVisible(true);
+  }
+
+  function closeChangePatient() {
+    setChangePatientVisible(false);
+  }
+
+  function onConfirmDate(params: { date: Date }) {
+    const newDate = formatDate(params.date);
+    if (date != newDate) {
+      setSelectedClinics(OrderedSet());
+    }
+    setDate(newDate);
+    closeChangeDate();
+  }
+
+  function onChangeSelected(selected: OrderedSet<Clinic["id"]>) {
+    setSelectedClinics(selected);
+  }
+
+  function onChangePatient(patient: Patient) {
+    setPatient(patient);
+    closeChangePatient();
   }
 
   return (
-    <PaperProvider theme={{ version: 2 }}>
-      <Toolbar
-        showSearchModal={showSearchModal}
-        hideSearchModal={hideSearchModal}
-        createClinic={async () => {
-          const title = prompt("Clinic name");
-          if (!title) {
-            return;
-          }
-
-          await createClinic(date, title);
-          mutateClinics();
-        }}
-        searchVisible={searchVisible}
-        setPatient={setPatient}
-      />
+    <PaperProvider
+      theme={{
+        ...DefaultTheme,
+        colors: materialColors.colors,
+      }}
+    >
       <View style={styles.container}>
-        <View>
-          <View>
-            <PatientInfo patient={patient} />
-          </View>
-          <Card style={styles.calendarContainer}>
-            <Card.Content>
-              <MiniCalender changeDate={updateDate} />
-            </Card.Content>
-          </Card>
-        </View>
-        <View style={styles.clinicContainer}>
-          {clinics?.map((clinic) => (
-            <View style={styles.clinicInfo}>
-              <ClinicInfo
-                clinic={clinic}
-                selectedPatient={patient}
-                onDelete={mutateClinics}
-              />
-            </View>
-          ))}
-        </View>
+        <ClinicToolbar
+          date={date}
+          selected={selectedClinics}
+          changeDate={openChangeDate}
+          onChangeSelected={onChangeSelected}
+        />
+        <DatePickerModal
+          locale="en-GB"
+          mode="single"
+          visible={changeDateVisible}
+          date={new Date(date)}
+          onConfirm={onConfirmDate as any}
+          onDismiss={closeChangeDate}
+        />
+        <PatientToolbar patient={patient} changePatient={openChangePatient} />
+        <ClinicViews
+          date={date}
+          selected={selectedClinics}
+          onChangeSelected={onChangeSelected}
+          patient={patient}
+          changePatient={openChangePatient}
+        />
+        <ChangePatientDialog
+          visible={changePatientVisible}
+          onChangePatient={onChangePatient}
+          onDismiss={closeChangePatient}
+        />
       </View>
     </PaperProvider>
   );
@@ -83,28 +104,6 @@ export default function Receptionist() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 15,
-    flexDirection: "row",
-  },
-
-  calendarContainer: {
-    marginRight: 15,
-    flex: 1,
-    backgroundColor: "white",
-  },
-
-  clinicContainer: {
-    flexDirection: "row",
-    marginLeft: 10,
-    flex: 1,
-  },
-  clinicInfo: {
-    flexGrow: 1,
-    flexShrink: 1,
-    flex: 1,
-  },
-  patientContainer: {
-    backgroundColor: "white",
+    ...(Platform.OS == "web" ? { height: "100vh" } : {}),
   },
 });
