605cfbfdfe5aed06b856f0a74afcd477f068be0e
Oleg Tretieu <ot321@ic.ac.uk>
2023-06-13 19:25:25 +0100

    Changing suggested time now displays correctly

--- components/staff/ChangeSuggestedDurationDialog.tsx
+++ components/staff/ChangeSuggestedDurationDialog.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { Button, Dialog, Portal, TextInput } from "react-native-paper";
 import DurationPicker from "./DurationPicker";
 import { Patient } from "../../lib/api";
@@ -7,6 +8,7 @@ export default function ChangeSuggestedDurationDialog(props: {
   visible: boolean;
   onDismiss: () => void;
 }) {
+  const [selectedValue, setSelectedValue] = useState(15);
   return (
     <Portal>
       <Dialog
@@ -16,7 +18,10 @@ export default function ChangeSuggestedDurationDialog(props: {
       >
         <Dialog.Title>Change suggested time for patient</Dialog.Title>
         <Dialog.Content>
-          <DurationPicker value={15} onChange={() => {}} />
+          <DurationPicker
+            value={selectedValue}
+            onChange={(value) => setSelectedValue(value)}
+          />
         </Dialog.Content>
         <Dialog.Actions>
           <Button onPress={props.onDismiss}>Cancel</Button>
