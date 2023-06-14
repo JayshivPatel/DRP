b73404d5cc8ff76f8fb7ffb308674f261a717d92
Oleg Tretieu <ot321@ic.ac.uk>
2023-06-14 11:59:13 +0100

    Linting fix

--- components/patient/SortAppointments.tsx
+++ components/patient/SortAppointments.tsx
@@ -5,7 +5,13 @@ export function sortAppointments(apps: Appointment[], reverse: boolean) {
     const date1 = app1.clinic?.date ? new Date(app1.clinic?.date) : new Date(0);
     const date2 = app2.clinic?.date ? new Date(app2.clinic?.date) : new Date(0);
 
-    return subtractTimeString(app1.startTime, app2.startTime, date1, date2, reverse);
+    return subtractTimeString(
+      app1.startTime,
+      app2.startTime,
+      date1,
+      date2,
+      reverse
+    );
   });
 
   return apps;
